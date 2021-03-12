a5ce5e318e8926ef243190f622862204b200a167
DoJeon1 <donghwan.dj@gmail.com>
2021-03-12 00:48:54 -0700

    added JS functionality for nav menu

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Adapted from online course "Build your own portfolio"
+// scrimba.com by Kevin Powell
+// code edits made by Daniel Jeon
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
