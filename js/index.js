5eb5305dd4071c551b349c351ddb6fb71a5088af
DoJeon1 <donghwan.dj@gmail.com>
2021-03-15 22:16:51 -0600

    Updated info, changed footer

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-// Adapted from online course "Build your own portfolio"
-// scrimba.com by Kevin Powell
-// code edits made by Daniel Jeon
-
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
