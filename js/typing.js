fa49e9ac946a578f101fc9822484ba9d11ff7e55
DoJeon1 <donghwan.dj@gmail.com>
2021-04-19 23:32:42 -0600

    added js script for typing effect of project page

--- js/typing.js
+++ js/typing.js
@@ -1,11 +1,11 @@
 
 var i = 0;
-var txt = 'STUDENT, ASPIRING DEV';
+var txt = 'Django Based Music Player';
 var speed = 140;
 
 function typeWriter() {
     if (i < txt.length) {
-        document.getElementById("intro__text").innerHTML += txt.charAt(i);
+        document.getElementById("about__project").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
