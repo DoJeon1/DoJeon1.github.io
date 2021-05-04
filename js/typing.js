91e9dddb8796f0f869bbb518bbf87a79caa829cf
DoJeon1 <donghwan.dj@gmail.com>
2021-05-04 00:14:16 -0600

    removed old code

--- js/typing.js
+++ js/typing.js
@@ -23,25 +23,4 @@ function typeWriter() {
     setTimeout(typeWriter, speed)
 }
 
-
-
-/* 
-var i = 0;
-var txt = 'STUDENT, ASPIRING DEV';
-var speed = 70;
-var backspace = false;
-
-var outputElem = document.getElementById("intro__text");
-function typeWriter() {
-  outputElem.textContent = txt.substring(0, i);
-
-  if (i > txt.length) backspace = true;
-  if (i == 0) backspace = false;
-  i += (backspace ? -1 : 1);
-  
-  setTimeout(typeWriter, speed);
-}
-
-*/
-
 typeWriter();
