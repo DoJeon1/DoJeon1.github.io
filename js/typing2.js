fc02a8d7b82e308a995c8494a4958b241ef76318
DoJeon1 <donghwan.dj@gmail.com>
2021-05-06 23:50:29 -0600

    changed the typewriter effect script for the second project page

--- js/typing2.js
+++ js/typing2.js
@@ -4,15 +4,24 @@
  var i = 0;
  var txt = 'Django Based Music Player';
  var speed = 140;
+ var backspace = false
  
  function typeWriter() {
-     if (i < txt.length) {
-         document.getElementById("about__project").innerHTML += txt.charAt(i);
+     document.getElementById("about__project").innerHTML = txt.substring(0, i);
+     if (!backspace) {
          i++;
-         setTimeout(typeWriter, speed);
      }
+     if (backspace) {
+         //i--;
+     }
+     if (i == txt.length) {
+         backspace = true;
+         clearTimeout(content);
+     }
+     if (i == 0) {
+         backspace = false;
+     }
+     content = setTimeout(typeWriter, speed)
  }
-
-
+ 
  typeWriter();
-
